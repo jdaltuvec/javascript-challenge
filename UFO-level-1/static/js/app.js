ce79c880d7f97939b93890a6355dd686bd1c4e6f
David A <josedavac@gmail.com>
2020-03-09 22:19:15 -0500

    app.js (basic) works. Page more styling in general.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
-const tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// Getting my variables ready
+const table = d3.select("#ufo-table")
+table.classed("table-stripped", true)
+let tbody = table.select("tbody")
+
+const input = d3.select("input")
+const button = d3.select("button")
+
+
+// Creating an event hadler for both the button and input
+const handler = function() {
+
+	let userInput = input.property("value")
+
+	tbody.html("")
+
+	tableData.filter(dataRow => dataRow.datetime === userInput).forEach(dataRow => {
+	let row = tbody.append("tr")
+	row.append("td").text(dataRow.datetime)
+	row.append("td").text(dataRow.city)
+	row.append("td").text(dataRow.state)
+	row.append("td").text(dataRow.country)
+	row.append("td").text(dataRow.shape)
+	row.append("td").text(dataRow.durationMinutes)
+	row.append("td").text(dataRow.comments)
+	})
+}
+
+
+input.on("change", handler);
+button.on("click", handler);
